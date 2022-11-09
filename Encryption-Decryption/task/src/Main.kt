336bf92c580b9004a10f80a88ae2d8d5fb36683a
Dev-Pasaka <dev.pasaka@gmail.com>
2022-11-10 01:35:25 +0300

    Encryption-Decryption

--- Encryption-Decryption/task/src/Main.kt
+++ Encryption-Decryption/task/src/Main.kt
@@ -1,50 +1,87 @@
 package encryptdecrypt
 
 fun main() {
+    val mode = readlnOrNull()!!
+    var text = readlnOrNull()!!
+    val key = readlnOrNull()!!.toInt()
+    when(mode){
+        "enc"-> encryption(text,key)
+        "dec"-> decryption(text,key)
+    }
 
-    /** PLAINTEXT INPUT8*/
-    val plainText = readlnOrNull()!!.toMutableList()
-    /** ENCRYPTION KEY*/
-    var key = readlnOrNull()!!.toInt()
+}
 
-    /** LIST OF ALPHABETS*/
-    val alphabetList = ('a'..'z').toMutableList()
-    val cypherTextIndexList = mutableListOf<Int>()
-    var cypherTextList = mutableListOf<Char>()
+fun encryption(text: String, key: Int){
+    val plainTextList = text.toMutableList()
+    val plainTextCharList = mutableListOf<Char>()
+    val unicodesList = ('!'..'~').toMutableList()
+    val unicodesCypherPositionList = mutableListOf<Int>()
     val list1 = mutableListOf<Int>()
-
-    for (text in plainText){
-        for (alpha in alphabetList){
-            if (text == alpha){
-                cypherTextIndexList.add(alphabetList.indexOf(text)+key)
+    val cypherTextList = mutableListOf<Char>()
+    for (char in plainTextList){
+        plainTextCharList.add(char)
+    }
+    for (text in plainTextCharList){
+        for (unicode in unicodesList){
+            if (text == unicode){
+                unicodesCypherPositionList.add(text.code+key)
             }
         }
         if (text.code == 32){
-            cypherTextIndexList.add(0)
+            unicodesCypherPositionList.add(32)
         }
     }
-    for (cypher in cypherTextIndexList){
-        if (cypher > 25){
-            list1.add(cypher.rem(26))
-        }else if (cypher == 0 ){
-           list1.add(cypher)
-        }
-        else{
+    for (cypher in unicodesCypherPositionList){
+        if (cypher > 126){
+            list1.add(cypher.rem(126))
+        }else{
             list1.add(cypher)
         }
     }
-    for (index in list1){
-        for (alpha in alphabetList){
-            if (alphabetList[index] == alpha ){
-                cypherTextList.add(alpha)
+    for(index in list1){
+        for (unicode in unicodesList){
+            if(unicode.code == index){
+                cypherTextList.add(unicode)
             }
         }
-        if(index == 0 ){
-            cypherTextList.add(' ')
-            cypherTextList.remove('a')
+        if(index == 32 ){
+            cypherTextList.add('%')
         }
-
     }
-    println(cypherTextList.joinToString(""))
+    val ciphertext = cypherTextList.joinToString("")
+    println(ciphertext)
+}
+fun decryption(text: String, key: Int){
+    val cipherText = text.toMutableList()
+    val cipherTextCharList = mutableListOf<Char>()
+    val unicodesList = ('!'..'~').toMutableList()
+    val plainTextPositionList = mutableListOf<Int>()
+    val plainTextList = mutableListOf<Char>()
+    for (cipher in cipherText){
+        cipherTextCharList.add(cipher)
+    }
 
+    for (cipher in cipherTextCharList){
+        for (unicode in unicodesList){
+            if (cipher == unicode){
+                plainTextPositionList.add(cipher.code-key)
+            }
+        }
+        if (cipher.code == 37){
+            plainTextPositionList.add(37)
+        }
+    }
+    for(index in plainTextPositionList) {
+        for (unicode in unicodesList) {
+            if (unicode.code == index) {
+                plainTextList.add(unicode)
+            }
+        }
+        if(index == 37 ){
+            plainTextList.add(' ')
+            plainTextList.remove('%')
+        }
+    }
+    val plainText = plainTextList.joinToString("")
+    println(plainText)
 }
